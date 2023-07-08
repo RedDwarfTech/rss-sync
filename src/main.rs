093cad3fa2b0d0793ff296537711ce9a8ce4a162
JiangXiaoqiang <jiangtingqiang@gmail.com>
2023-07-09 00:40:13 +0800

    refactor: change the task trigger style

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,36 @@
 #![allow(non_upper_case_globals)]
+extern crate openssl;
 
-use env_logger::Env;
-use rss::celery::celery_init::init_impl;
 use log::{info, error};
+use std::time::Duration;
+use tokio::{select, time};
+
+use crate::rss::{celery::celery_init::init_impl, models::appenum::celery_opt::CeleryOpt};
 
 pub mod rss;
 
 #[tokio::main]
 async fn main() {
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
-    let r = init_impl().await;
-    match r {
-        Ok(_) => {
-            info!("initial celery success");
-        },
-        Err(err) => {
-            error!("initial celery failed:{}",err);
-        },
+    let mut interval1 = time::interval(Duration::from_secs(15));
+    let mut interval2 = time::interval(Duration::from_secs(30));
+
+    loop {
+        select! {
+                    _ = interval1.tick() => {
+                        // 执行任务1
+                        let result = init_impl(CeleryOpt::Consume).await;
+                        match result {
+                            Ok(_) => {
+                                info!("schedule task success")
+                            },
+                            Err(e) => {
+                                error!("schedule task failed, {}",e);
+                            },
+        }
+                    }
+                    _ = interval2.tick() => {
+                        // 执行任务2
+                    }
+                }
     }
 }
--- src/rss/celery/celery_init.rs
+++ src/rss/celery/celery_init.rs
@@ -1,70 +1,31 @@
-use std::{time::Duration};
-
-use celery::{task::{TaskResult, TaskResultExt}};
+use celery::task::TaskResult;
 use log::info;
-use structopt::StructOpt;
-use tokio::time::{self};
 
 use crate::rss::models::appenum::celery_opt::CeleryOpt;
 
-// This generates the task struct and impl with the name set to the function name "add"
 #[celery::task]
 fn add(x: i32, y: i32) -> TaskResult<i32> {
-    info!("consumed message:{}{}",x,y);
+    info!("consumed message:{}{}", x, y);
     Ok(x + y)
 }
 
-// Demonstrates a task that raises an error, and also how to customize task options.
-// In this case we override the default `max_retries`.
-#[celery::task(max_retries = 3)]
-async fn buggy_task() -> TaskResult<()> {
-    let data = tokio::fs::read("this-file-doesn't-exist")
-        .await
-        .with_unexpected_err(|| {
-            "This error is part of the example, it is used to showcase error handling"
-        })?;
-    println!("Read {} bytes", data.len());
-    Ok(())
-}
-
-// Demonstrates a long running IO-bound task. By increasing the prefetch count, an arbitrary
-// number of these number can execute concurrently.
-#[celery::task(max_retries = 2)]
-async fn long_running_task(secs: Option<u64>) {
-    let secs = secs.unwrap_or(10);
-    time::sleep(Duration::from_secs(secs)).await;
-}
-
-// Demonstrates a task that is bound to the task instance, i.e. runs as an instance method.
-#[celery::task(bind = true)]
-fn bound_task(task: &Self) {
-    // Print some info about the request for debugging.
-    println!("{:?}", task.request.origin);
-    println!("{:?}", task.request.hostname);
-}
-
-pub async fn init_impl() -> Result<(), Box<dyn std::error::Error>>{
-    let opt = CeleryOpt::from_args();
-    let redis_addr = std::env::var("REDIS_ADDR").unwrap_or_else(|_| "redis://127.0.0.1:6379/".into());
-    info!("redis addr:{}",redis_addr);
+pub async fn init_impl(opt: CeleryOpt) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    let redis_addr =
+        std::env::var("REDIS_ADDR").unwrap_or_else(|_| "redis://127.0.0.1:6379/".into());
+    info!("redis addr:{}", redis_addr);
     let my_app = celery::app!(
         broker = RedisBroker { redis_addr },
-        // broker = AMQPBroker { std::env::var("AMQP_ADDR").unwrap_or_else(|_| "amqp://127.0.0.1:5672".into()) },
         tasks = [
-            add,
-            buggy_task,
-            long_running_task,
-            bound_task,
+            add
         ],
-        // This just shows how we can route certain tasks to certain queues based
-        // on glob matching.
         task_routes = [
             "buggy_task" => "buggy-queue",
             "*" => "celery",
         ],
         prefetch_count = 2,
         heartbeat = Some(10)
-    ).await?;
+    )
+    .await?;
 
     match opt {
         CeleryOpt::Consume => {
@@ -73,35 +34,11 @@ pub async fn init_impl() -> Result<(), Box<dyn std::error::Error>>{
         }
         CeleryOpt::Produce { tasks } => {
             if tasks.is_empty() {
-                // Basic task sending.
                 my_app.send_task(add::new(1, 2)).await?;
-                my_app.send_task(bound_task::new()).await?;
-
-                // Sending a task with additional options like `countdown`.
-                my_app.send_task(add::new(1, 3).with_countdown(3)).await?;
-
-                // Send the buggy task that will fail and be retried a few times.
-                my_app.send_task(buggy_task::new()).await?;
-
-                // Send the long running task that will fail with a timeout error.
-                my_app
-                    .send_task(long_running_task::new(Some(3)).with_time_limit(2))
-                    .await?;
-                // Send the long running task that will succeed.
-                for _ in 0..100 {
-                    my_app
-                        .send_task(long_running_task::new(Some(10)).with_time_limit(20))
-                        .await?;
-                }
             } else {
                 for task in tasks {
                     match task.as_str() {
                         "add" => my_app.send_task(add::new(1, 2)).await?,
-                        "bound_task" => my_app.send_task(bound_task::new()).await?,
-                        "buggy_task" => my_app.send_task(buggy_task::new()).await?,
-                        "long_running_task" => {
-                            my_app.send_task(long_running_task::new(Some(3))).await?
-                        }
                         _ => panic!("unknown task"),
                     };
                 }
