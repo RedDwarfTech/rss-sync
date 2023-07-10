f921b897e023489e2e4c5ce55cc6ce9116ac3b1a
JiangXiaoqiang <jiangtingqiang@gmail.com>
2023-07-11 03:14:32 +0800

    refactor: twaek

--- scripts/startup-app.sh
+++ scripts/startup-app.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-nohup ./rss-sync consume 2>&1 >> consume.log </dev/null &
-nohup ./rss-sync produce add 2>&1 >> produce.log </dev/null &
+nohup ./rss-sync consume >> consume.log </dev/null &
+nohup ./rss-sync produce add >> produce.log </dev/null &
