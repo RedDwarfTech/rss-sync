c31ff71d59a436bacaf9a1448ba254e0df49a9a3
jiang qiang <jiangtingqiang@gmail.com>
2023-07-11 02:07:14 +0800

    refactor: tweak the shell of startup script

--- scripts/startup-app.sh
+++ scripts/startup-app.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-nohup ./rss-sync consume >> consume.log &
-nohup ./rss-sync produce add >> produce.log &
+nohup ./rss-sync consume >> consume.log 2>&1 </dev/null &
+nohup ./rss-sync produce add >> produce.log 2>&1 </dev/null &
