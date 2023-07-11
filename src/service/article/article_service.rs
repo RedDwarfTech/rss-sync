e89054427030342b24a358db82e712aaa26bc9d8
JiangXiaoqiang <jiangtingqiang@gmail.com>
2023-07-11 17:31:38 +0800

    refactor: tweak the insert constraint column

--- src/service/article/article_service.rs
+++ src/service/article/article_service.rs
@@ -8,7 +8,7 @@ pub fn insert_article(input_article: &AddArticle) -> Result<Article, diesel::res
     use crate::model::diesel::dolphin::dolphin_schema::article::dsl::*;
     let result = diesel::insert_into(article)
         .values(input_article)
-        .on_conflict(title)
+        .on_conflict((title,sub_source_id))
         .do_update()
         .set((title.eq("unknow"),))
         .get_result::<Article>(&mut get_connection());
