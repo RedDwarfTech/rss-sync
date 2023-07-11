80b568f87ad1b7324d2cfc3adcb7698fdef47f48
JiangXiaoqiang <jiangtingqiang@gmail.com>
2023-07-11 13:36:52 +0800

    refactor: tweak log output

--- scripts/startup-app.sh
+++ scripts/startup-app.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
-nohup ./rss-sync consume >> consume.log &
-nohup ./rss-sync produce add >> produce.log &
+nohup ./rss-sync consume >> consume.log 2>&1 &
+nohup ./rss-sync produce add >> produce.log 2>&1 &
+
+tail -f produce.log
