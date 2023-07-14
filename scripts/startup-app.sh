57c57891ec7770534a1bce1bfd793ab99a5eaa77
JiangXiaoqiang <jiangtingqiang@gmail.com>
2023-07-14 23:47:36 +0800

    refactor: tweak

--- scripts/startup-app.sh
+++ scripts/startup-app.sh
@@ -1,2 +1,6 @@
-nohup ./rss-sync consume >> consume.log &
-nohup ./rss-sync produce add >> produce.log &
+#!/bin/sh
+
+nohup ./rss-sync consume >> consume.log 2>&1 &
+nohup ./rss-sync produce add >> produce.log 2>&1 &
+
+tail -f produce.log
