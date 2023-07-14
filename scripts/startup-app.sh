a2dec154153d6f70b4bb8fffd793e5cda0b6229d
JiangXiaoqiang <jiangtingqiang@gmail.com>
2023-07-14 23:46:16 +0800

    feat: tweak the startup script

--- scripts/startup-app.sh
+++ scripts/startup-app.sh
@@ -1,6 +1,2 @@
-#!/bin/sh
-
-nohup ./rss-sync consume >> consume.log 2>&1 &
-nohup ./rss-sync produce add >> produce.log 2>&1 &
-
-tail -f produce.log
+nohup ./rss-sync consume >> consume.log &
+nohup ./rss-sync produce add >> produce.log &
