12283d9f2ca050e1d67236b40c6ee49e2c22d517
JiangXiaoqiang <jiangtingqiang@gmail.com>
2023-09-06 10:17:23 +0800

    feat: merge the two process to threads

--- scripts/startup-app.sh
+++ scripts/startup-app.sh
@@ -2,7 +2,6 @@
 
 export MALLOC_CONF="prof:true,prof_prefix:jeprof.out"
 
-nohup ./rss-sync consume >> consume.log 2>&1 &
-nohup ./rss-sync produce add >> produce.log 2>&1 &
+nohup ./rss-sync >> rss-sync.log 2>&1 &
 
-tail -f produce.log
+tail -f rss-sync.log
--- src/main.rs
+++ src/main.rs
@@ -6,23 +6,35 @@ extern crate diesel;
 use crate::cruise::models::appenum::celery_opt::CeleryOpt;
 use cruise::celery::celery_init::init_impl;
 use cruise::sched::scheduler::check_tpl_task;
-use env_logger::Env;
-use structopt::StructOpt;
+use std::thread;
 
+pub mod cache;
 pub mod common;
 pub mod cruise;
 pub mod model;
 pub mod service;
-pub mod cache;
 
 #[tokio::main]
 async fn main() {
-    handle_task().await;
+    let produce_thread = thread::spawn(|| {
+        tokio::runtime::Runtime::new().unwrap().block_on(async {
+            let tasks = vec!["add".to_string()];
+            handle_task(&CeleryOpt::Produce { tasks }).await;
+        });
+    });
+
+    let consume_thread = thread::spawn(|| {
+        tokio::runtime::Runtime::new().unwrap().block_on(async {
+            handle_task(&CeleryOpt::Consume).await;
+        });
+    });
+
+    produce_thread.join().unwrap();
+    consume_thread.join().unwrap();
 }
 
-async fn handle_task() {
+async fn handle_task(opt: &CeleryOpt) {
     log4rs::init_file("log4rs.yaml", Default::default()).unwrap();
-    let opt = CeleryOpt::from_args();
     match &opt {
         CeleryOpt::Consume => {
             let _result = init_impl(&opt).await;
